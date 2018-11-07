8d02d034df601057121726307d0455a701a9ccaa
Austin Gordon <gordon.austin2@gmail.com>
2018-11-07 15:55:34 -0600

    Init

---
+++ README.md
@@ -0,0 +1,20 @@
+# copy-to-range-cli
+
+Copy files to a range of directories.
+
+## Installation
+
+`npm install --save copy-to-range-cli`
+
+## Related
+
+- [copy-to-range](https://github.com/hutsoninc/copy-to-range) - API for this module
+- [make-dir-range-cli](https://github.com/hutsoninc/make-dir-range-cli) - Make directories from a range of integers
+
+## Authors
+
+* **Austin Gordon** - *Development* - [GitHub](https://github.com/AustinLeeGordon)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ cli.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+'use strict';
+const meow = require('meow');
+const copyToRange = require('copy-to-range');
+
+const cli = meow(`
+	Usage
+	  $ copy-to-range <file> <int>-<int>
+	Options
+      --destination, -d  Destination for dirs
+      --prepend, -p  Prepend to destination dir names
+      --append, -a  Append to destination dir names
+	Example
+	  $ copy-to-range file.txt 1-5,7,9-10
+`, {
+    flags: {
+        append: {
+            type: 'string',
+            default: '',
+            alias: 'a'
+        },
+        prepend: {
+            type: 'string',
+            default: '',
+            alias: 'p'
+        },
+        destination: {
+            type: 'string',
+            default: '',
+            alias: 'd'
+        }
+    }
+});
+
+const input = cli.input;
+
+if (input.length === 0) {
+    console.error('No args provided');
+    process.exit(1);
+}
+
+if (input.length === 1) {
+    console.error('No range provided');
+    process.exit(1);
+}
+
+copyToRange(input[0], input[1], cli.flags)
